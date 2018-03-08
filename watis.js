08a184876c02a5dc41ca9fb097ae9fd5be85a2be
laurensdelombaert <laurens.delombaert@outlook.be>
2018-03-09 00:14:23 +0100

    Added pug files to watis.js

--- watis.js
+++ watis.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+const pug = require('pug');
+
+const watIs = pug.compileFile('watis.pug');
+const nav = pug.compileFile('nav.pug');
+
 const config = {
     port: 3000,
 };
 
 app.get('/watis', (req, res) => {
-    res.send('Hello World')
+    res.setHeader('Content-Type', 'text/html');
+    res.write(watIs());
+    res.write(nav());
+    res.end();
 });
 
 app.listen(config.port, () => {
