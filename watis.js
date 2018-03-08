b7054aed0f40e25e50f39c167e8e6c3340ae608f
laurensdelombaert <laurens.delombaert@outlook.be>
2018-03-09 00:41:46 +0100

    Attempted fixing images

--- watis.js
+++ watis.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 const pug = require('pug');
 
