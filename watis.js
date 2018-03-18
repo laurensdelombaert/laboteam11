6418b2e499d89cdd61bdbdf3e1e0186c968ee5c2
Laurens De Lombaert <laurens.delombaert@outlook.be>
2018-03-18 10:25:30 +0100

    Reworked pug into view engine

--- watis.js
+++ watis.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 app.use("/public", express.static(path.join(__dirname, 'public')));
-
-const pug = require('pug');
-
-const watIs = pug.compileFile('watis.pug');
+app.set('view engine', 'pug');
 
 const config = {
     port: process.env.PORT || 3000,
@@ -14,12 +12,13 @@ const config = {
 app.use(express.static('public'));
 
 app.get('/kweenie/wat-is-express-js-en-waarvoor-dient-het', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write(watIs({
+    
+    res.render("watis", {
         avatar: "user8.png",
         user: "Anneke Kodeur",
         likes: 20
-    }));
+    });
+    
     res.end();
 });
 
